c1e36872f385b30300552c2713bd18da6772a5ab
AnErrupTion <anerruption@disroot.org>
2022-07-14 14:03:01 +0200

    Improve IR compiler performance

--- NativeCIL/IR/IRCompiler.cs
+++ NativeCIL/IR/IRCompiler.cs
@@ -53,17 +53,12 @@ public class IRCompiler
                     continue;
                 }
 
-                var branches = GetAllBranches(method).ToList();
                 AddInstruction(Label, -1, GetSafeName(method.FullName));
 
                 foreach (var inst in method.Body.Instructions)
                 {
-                    foreach (var branch in branches)
-                        if (((Instruction)branch.Operand).Offset == inst.Offset)
-                        {
-                            AddInstruction(Label, -1, BrLabelName(inst, method, true));
-                            break;
-                        }
+                    if (inst.OpCode.OperandType is OperandType.InlineBrTarget or OperandType.ShortInlineBrTarget)
+                        AddInstruction(Label, -1, BrLabelName(inst, method, true));
 
                     AddInstruction(Comment, -1, inst.OpCode);
                     switch (inst.OpCode.Code)
@@ -363,13 +358,6 @@ public class IRCompiler
 
     private static string BrLabelName(Instruction ins, MethodDef def, bool create = false) =>
         $"LB_{def.GetHashCode():X4}{(create ? ins.Offset : ((Instruction)ins.Operand).Offset):X4}";
-
-    private static IEnumerable<Instruction> GetAllBranches(MethodDef method)
-    {
-        foreach (var br in method.Body.Instructions)
-            if (br.OpCode.OperandType is OperandType.InlineBrTarget or OperandType.ShortInlineBrTarget)
-                yield return br;
-    }
 
     private void AddInstruction(IROpCode opCode, int flags = 0, object? operand1 = null, object? operand2 = null) =>
         Instructions.Add(new IRInstruction(opCode, flags, operand1, operand2));
